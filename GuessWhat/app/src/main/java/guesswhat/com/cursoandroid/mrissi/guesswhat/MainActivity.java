9e75d784de867d309db711fc914ad5aa2f603011
Maisa Rissi <maisa.rissi@gmail.com>
2017-09-03 18:52:51 -0300

    first android app GuessWhat

---
+++ GuessWhat/app/src/main/java/guesswhat/com/cursoandroid/mrissi/guesswhat/MainActivity.java
@@ -0,0 +1,34 @@
+package guesswhat.com.cursoandroid.mrissi.guesswhat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPlay;
+    private TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonPlay = (Button) findViewById(R.id.button_play_id);
+        resultText = (TextView) findViewById(R.id.txt_result_id);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int randomNumber = random.nextInt(11);
+
+                resultText.setText("" + randomNumber);
+            }
+        });
+    }
+}
